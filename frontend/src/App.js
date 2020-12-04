7b389aac049d81b79be49d03ff730da2aac2244d
FelixHoangVo <felix@vinova.com.sg>
2020-12-04 08:34:10 +0700

    innit commit

---
+++ frontend/src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import {Grid, Typography, Button} from '@material-ui/core'
+import PlayerHolder from './components/playerHolder'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  rootTitle: {
+    fontSize: '30px',
+    color: '#fff',
+    textAlign: 'center'
+  },
+  rootGrid: { 
+    minHeight: '90vh'
+  }
+});
+
+function App() {
+  const classes = useStyles();
+  return (
+    <div className="App">
+      <Typography classes={{root: classes.rootTitle}}>Welcome to the game</Typography>
+      
+      <Grid container classes={{root: classes.rootGrid}} direction='row' justify='space-around' alignItems='center'>
+        <Grid item>
+          <PlayerHolder/>
+          <PlayerHolder/>
+          
+        </Grid>
+        <Grid item>
+          <PlayerHolder/>
+          <PlayerHolder/>
+          
+        </Grid>
+        <Grid item>
+          <PlayerHolder/>
+          <PlayerHolder/>
+        </Grid>
+      </Grid>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/playerHolder/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Alert from '@material-ui/lab/Alert';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    marginTop: 100,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  name: {
+    marginBottom: 12,
+  },
+});
+
+export default function SimpleCard() {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Player 1
+        </Typography>
+        <Typography className={classes.name} color="textSecondary">
+          Player Name
+        </Typography>
+        <Alert severity="success">Ready</Alert>
+      </CardContent>
+    </Card>
+  );
+}
